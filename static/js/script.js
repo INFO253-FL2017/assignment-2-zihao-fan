54f30724c8d6d3bce0a4c41e2f7688f4e83970f3
zihao-fan <fanzihao.thu@gmail.com>
2017-10-05 20:19:42 +0000

    converted from static

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,23 @@
-// script.js
+function checkContact() {
+  var email_input = document.getElementById("exampleEmailInput");
+  var subject_input = document.getElementById("exampleSubjectInput");
+  var message_input = document.getElementById("exampleMessage");
+  var alter_message = document.getElementById("alertMessage");
+  if (email_input.value.length == 0) {
+    alter_message.innerHTML = "Email cannot be empty";
+    return false;
+  } else if (subject_input.value.length == 0) {
+    alter_message.innerHTML = "Subject cannot be empty!";
+    return false;
+  } else if (message_input.value.length == 0) {
+    alter_message.innerHTML = "Message cannot be empty!";
+    return false;
+  } else {
+    alter_message.innerHTML = "Your message has been sent!";
+    return false;
+  }
+}
 
-console.log("js/script.js has been loaded!");
+function submitComment() {
+  return false;
+}
--- webserver.py
+++ webserver.py
@@ -8,7 +8,7 @@ from flask import Flask, render_template
 
 # Create application, and point static path (where static resources like images, css, and js files are stored) to the
 # "static folder"
-app = Flask(__name__,static_url_path="/static")
+app = Flask(__name__, static_url_path="/static")
 
 @app.route('/')
 def hello_world():
@@ -17,3 +17,15 @@ def hello_world():
     returns will be sent to the browser
     """
     return render_template("index.html") # Render the template located in "templates/index.html"
+
+@app.route('/<page_name>')
+def show_top_pages(page_name):
+    if not page_name.endswith('.html'):
+        page_name += '.html'
+    return render_template(page_name)
+
+@app.route('/blog/<blog_title>')
+def show_blog_pages(blog_title):
+    if not blog_title.endswith('.html'):
+        blog_title += '.html'
+    return render_template('/blog/' + blog_title)
